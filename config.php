5a03ae4817c4a5883319a6497c3e8caf2c4613ff
migaldottir <none399@outlook.com>
2023-05-15 23:10:34 +0200

    added comments in config file, defined Select function in index.php

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
 class Connect extends PDO 
+//it extends PDO class
 {
 
     public function __construct()
+    //below is all the data needed for you to be able to connect to the db
     {
         parent::__construct("mysql:host=localhost;dbname=primo+api", "root", " ",
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $this->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    //root is the username, '' is the place for password
+    //$this is in case there is no such user, password or DB
     }
     }
 ?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once__DIR__ . '/config.php';
+class API {
+    function Select() {
+        $db = new Connect;
+        $users = array();
+        $data = $db-> prepare(`SELECT * users ORDER BY id`);
+        $data->execute();
+        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
+            //associating the data from DB to the users variable
+        $users[$OutputData["id"]] = array(
+            "id" => $OutputData["id"],
+            "name" => $OutputData["name"],
+            "age" => $OutputData["age"]
+        );
+        }
+    }
+}
